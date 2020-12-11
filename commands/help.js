0ea59ed56e774fe5f9358e01ea9e1955e1215166
Nozi.4po <58081825+Nozi-4PO@users.noreply.github.com>
2020-12-11 23:39:44 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -4,10 +4,10 @@ module.exports.run = async(bot, message, args) => {
 
     var embed = new Discord.RichEmbed()
 
-    .setTitle(`:shield: __**Someday sécurisé!**__`)
-    .setThumbnail("https://image.noelshack.com/fichiers/2019/46/6/1573906701-helppp.png")
+    .setTitle(`:shield: __**Synthwave Sécurisé!**__`)
+    .setThumbnail("https://images-ext-2.discordapp.net/external/oL0wupGsPduC9ZTRFp7PVetPE8VdxsHb26P2jvnp8pM/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/773902764227952722/7c4993cef2b0133cf72de7e720dbf629.webp?width=369&height=369")
     .addField("**Protection :shield: **", "`antibot, antispam, botlist, locktime, blacklist`")
-    .setFooter("Créateurs du bot : Douze#0001")
+    .setFooter("Créateurs du bot : ! " X-Nozi#0777")
   
    
   
